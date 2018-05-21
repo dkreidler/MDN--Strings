bcf9002851790a127c07001a1d91b4c16ab06541
dkreidler <dkreidler@gmail.com>
2018-05-20 21:56:01 -0400

    added interactivity, display reset

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var button2 = document.querySelector('p');
 button.onclick = function() {
     var name = prompt('What is your name?');
     document.getElementById('response').innerHTML = 'Hello ' + name + ', nice to see you!';
+    document.getElementById('response').style.display = 'inline-block';
 };
 button2.onclick = function() {
     document.getElementById('response').innerHTML = "";
+    document.getElementById('response').style.display = "none";
 };
